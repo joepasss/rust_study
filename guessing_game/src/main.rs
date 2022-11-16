32b9144606078b0d4bfdc183a468ac28e9adbc7c
Miller <farwes1180@gmail.com>
2022-11-16 18:23:16 +0900

    .gitignore update & app init

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,3 @@
-use std::io;    // to obtain user input and then print the result as output "io" input/output libray into scope, "io" library comes from the standard library, known as std:
-
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    let mut guess= String::new();   // new varialbe that mutable and has empty string value
-
-    io::stdin()
-        .read_line(&mut guess)      // & == references, so mutate variables
-        .expect("Failed to red line");                  // handling potential Failure with the Result type
-
-    println!("You guessed: {guess}");
+    println!("Hello, world!");
 }
-
-/*
-    Variables!
-    let apples = 5;         immutable
-    let mut bananas = 5;    mutable
-*/
