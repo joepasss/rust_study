de8eda5020a20e983fe5504a3a19fe70643633ff
Miller <farwes1180@gmail.com>
2022-11-17 21:54:18 +0900

    spell check

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -21,14 +21,14 @@ fn main() {
     // difference between constants and variables
     // allowed to use mut with constants
     // the type of the value must be annotated.
-    // const can be any scope (let is cannot assined in global scope)
-    // that constants may be set only to a constnt expression, not the result of a value that could only be computed at runtime
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // const can be any scope (let is cannot assigned in global scope)
+    // that constants may be set only to a constant expression, not the result of a value that could only be computed at runtime
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
     /* Shadowing */
     // you can declare a new variable with the same name as a previous variable.
     // "first variable is shadowed by the second"
-    // that the second variable is what the compiler will see when you use the name of the vraiable, in effect, the second variable overshadows the first, taking any uses of the variable name to itself until either it itself is shadowed or the scope ends. We can shadow a variable by using the same variable's name and repeating the sue of the let keyword
+    // that the second variable is what the compiler will see when you use the name of the variable, in effect, the second variable overshadows the first, taking any uses of the variable name to itself until either it itself is shadowed or the scope ends. We can shadow a variable by using the same variable's name and repeating the sue of the let keyword
 
     let s = 5;
     let s = s + 1;
@@ -41,5 +41,5 @@ fn main() {
     println!("The value of s is: {s}"); // 6
 
     // shadowing is different from marking a variable as mut.
-    // by using let, we can perform a few transformations on a value but have the variable be immutable after those transformations have been compeleted.
+    // by using let, we can perform a few transformations on a value but have the variable be immutable after those transformations have been completed.
 }
