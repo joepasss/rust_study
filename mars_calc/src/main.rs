f385253abfd28c315cb6f96bf6dcfea18ab84107
Miller <farwes1180@gmail.com>
2022-12-18 23:03:03 +0900

    ownership

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,8 +1,16 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    let input = String::new(); // owned by input variable (heap)
+    let mut s = input; // input is no longer valid because string is complex type that value is cannot be copy it only can move.
+
+    /*
+    let a = 5;
+    let b = a;  // a's value is copied
+    println!("{}", b);
+    */
+
+    io::stdin().read_line(&mut s);
     let mars_weight: f32 = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {}kg", mars_weight);
 }
@@ -10,3 +18,11 @@ fn main() {
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
+
+// OWNERSHIP
+
+// 1. Each value in Rust is owned by a variable.
+
+// 2. When the owner goes out of scope, the value will be deallocated.
+
+// 3. There can only be ONE owner at a time.
