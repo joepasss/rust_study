aa690a7be3387f627353034286f2a40b709f8318
Miller <farwes1180@gmail.com>
2022-12-21 06:41:03 +0900

    server struct init

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Server running in {}", self.addr);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8000".to_string());
+    server.run();
 }
