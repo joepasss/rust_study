a262ec0b229d08d7381002007530190693b38d61
Miller <farwes1180@gmail.com>
2022-12-24 17:52:24 +0900

    separte files

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,55 +1,11 @@
-use http::request::Request;
+use http::Method;
+use http::Request;
 use server::Server;
+
+mod http;
+mod server;
 
 fn main() {
     let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
-
-        pub fn run(self) {
-            println!("Server running in {}", self.addr);
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-
-    pub mod method {
-        pub enum Method {
-            GET,
-            DELETE,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
-}
-
-/*
-    GET /user?id=10 HTTP/1.1\r\n
-    HEADERS \r\n
-    BODY
-*/
