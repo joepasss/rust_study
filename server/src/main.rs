cd96f6b59c2a1a4d414d4f4df6228393602af9b0
Miller <farwes1180@gmail.com>
2022-12-24 17:46:27 +0900

    modules

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,43 +1,51 @@
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Server running in {}", self.addr);
-    }
-}
+use http::request::Request;
+use server::Server;
 
 fn main() {
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
-
     let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Server running in {}", self.addr);
+        }
+    }
 }
 
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+mod http {
+    pub mod request {
+        use super::method::Method;
+
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
 
 /*
