4912bb023e2a1422740a2a2c73643f36a82d9d2a
Miller <farwes1180@gmail.com>
2022-12-27 08:35:27 +0900

    trait

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,7 +1,39 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let string = String::from("sdf");
+        string.encrypt();
+        buf.encrypt();
+        unimplemented!()
+    }
+}
+
+trait Encrypt {
+    fn encrypt(&self) -> Self;
+}
+
+impl Encrypt for String {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Encrypt for &[u8] {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
 }
