ff9fee3b33cb108c047c270d2f5e7c3651e92b2c
Miller <farwes1180@gmail.com>
2023-01-01 01:26:02 +0900

    custom Error

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,9 @@
 use super::method::Method;
-use std::convert::TryFrom;
+use std::{
+    convert::TryFrom,
+    error::Error,
+    fmt::{Debug, Display, Formatter, Result as FmtResult},
+};
 
 pub struct Request {
     path: String,
@@ -7,10 +11,48 @@ pub struct Request {
     method: Method,
 }
 
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
