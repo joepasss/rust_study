6a75aa76719c418c38dae14d19b5c37aa462cc0d
Miller <farwes1180@gmail.com>
2023-01-01 22:28:26 +0900

    parse string value

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::{
     convert::TryFrom,
     error::Error,
@@ -33,6 +33,8 @@ impl TryFrom<&[u8]> for Request {
             return Err(ParseError::InvalidProtocol);
         }
 
+        let method: Method = method.parse()?;
+
         unimplemented!()
     }
 }
@@ -62,6 +64,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
