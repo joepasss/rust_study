4df2fa61d69dede4125ce72d9ca05a7f4b101d11
Miller <farwes1180@gmail.com>
2023-01-01 23:18:16 +0900

    cleanup

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -36,23 +36,6 @@ impl TryFrom<&[u8]> for Request {
 
         let method: Method = method.parse()?;
 
-        let mut query_string = None;
-        match path.find('?') {
-            Some(i) => {
-                query_string = Some(&path[i + 1..]);
-                path = &path[..i];
-            }
-            None => {}
-        }
-
-        let q = path.find("?");
-
-        if q.is_some() {
-            let i = q.unwrap();
-            query_string = Some(&path[i + 1..]);
-            path = &path[..i];
-        }
-
         if let Some(i) = path.find('?') {
             query_string = Some(&path[i + 1..]);
             path = &path[..i];
