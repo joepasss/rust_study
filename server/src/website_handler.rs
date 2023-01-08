6ad1dd0659a77ab30a061157e7c31ba40bc654d8
Miller <farwes1180@gmail.com>
2023-01-08 22:37:47 +0900

    handling env

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -2,7 +2,15 @@ use super::{
     http::{Method, Request, Response, StatusCode},
     server::Handler,
 };
-pub struct WebSiteHandler;
+pub struct WebSiteHandler {
+    public_path: String,
+}
+
+impl WebSiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebSiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
