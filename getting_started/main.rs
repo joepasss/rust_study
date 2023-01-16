17d82e9149540f2dc94f5a97ba43b9b801e57029
Miller <farwes1180@gmail.com>
2023-01-16 17:13:15 +0900

    review branch init

--- getting_started/main.rs
+++
@@ -1,6 +0,0 @@
-// define a function named main
-// main function is always the first code that runs in every excutable Rust program
-
-fn main() {
-  println!("Hello World!");  // println! calls a Rust macro. without "!" is function
-}
