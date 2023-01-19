4f636114823f1474d80d63ae6f73152c34c797b6
Miller <farwes1180@gmail.com>
2023-01-20 06:27:02 +0900

    mars calc v2

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use std::num::ParseFloatError;
+
 fn main() {
-    println!("HELLO!");
+    loop {
+        let mut input = String::new();
+        println!("Enter your weight! (kg): ");
+        io::stdin().read_line(&mut input).unwrap();
+
+        let weight: Result<f32, ParseFloatError> = input.trim().parse();
+
+        match weight {
+            Ok(weight) => {
+                println!("Weight on mars: {}", calculate_weight_on_mars(weight));
+
+                break;
+            }
+            Err(e) => {
+                println!("Please input valid number");
+
+                println!("ERR: {}", e);
+                continue;
+            }
+        };
+    }
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
