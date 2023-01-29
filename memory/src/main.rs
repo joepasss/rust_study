434e41661905c42922ec4a39c68e17d22a9993a9
Miller <farwes1180@gmail.com>
2023-01-30 00:38:05 +0900

    smart pointers

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,9 +1,5 @@
-// THE HEAP
-// It's a region of the process memory that is NOT automatically managed
-// we should manually allocate and free
-// it has no size restrictions
-// It's accessible by any function, anywhere in the program.
-// Heap allocations are expensive and we should avoid them when possible
+// SMART POINTERS
+// additional row pointer wrapper
 
 fn main() {
     let a = 2;
@@ -16,14 +12,5 @@ fn stack_only(i32 b) {
 
 fn stack_and_heap(i32 a) {
     let d = 5;
-    // SOME HEAP VAR V
+    // SOME HEAP VAR V      // if "stack_and_heap" function is end, deallocated automatically (smart pointer)
 }
-
-
-// heap
-
-// 0xf123123 => some addr
-// 7 => some value
-
-// STACK
-// V = 0xf123123 => in stack we store only address
