603dfc727b4d473426295b3c6eda92637336818a
Miller <farwes1180@gmail.com>
2023-01-30 00:35:19 +0900

    the heap

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,7 +1,9 @@
-// STACK
-// special region of the process memory that stores variables created by each function
-// for every function call a new stack frame is allocated on top of the current one.
-// the size of every variable on the stack has to be known at compile time
+// THE HEAP
+// It's a region of the process memory that is NOT automatically managed
+// we should manually allocate and free
+// it has no size restrictions
+// It's accessible by any function, anywhere in the program.
+// Heap allocations are expensive and we should avoid them when possible
 
 fn main() {
     let a = 2;
@@ -12,14 +14,16 @@ fn stack_only(i32 b) {
     let c = 3;
 }
 
-// stack
+fn stack_and_heap(i32 a) {
+    let d = 5;
+    // SOME HEAP VAR V
+}
 
-// stack_only
-// b = 2
-// c = 3
-// has to store two value 
 
-// main
-// a = 2
+// heap
 
-// stack has limited size!
+// 0xf123123 => some addr
+// 7 => some value
+
+// STACK
+// V = 0xf123123 => in stack we store only address
